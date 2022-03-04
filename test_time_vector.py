22e9f9a185d776d66bdb6dfeb73eed42df7df203
장병희 <jmkabc31@ajou.ac.kr>
2022-03-04 17:02:41 +0900

    change color and value

--- test_time_vector.py
+++ test_time_vector.py
@@ -182,10 +182,9 @@ def detect(opt, sync, cam_num, record):  # Homography 매칭에 사용되는 행
 
                             x, y, w, h = xyxy_[0], xyxy_[1], xyxy_[2], xyxy_[3]
                             plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=2)
-                            cv2.putText(im0, str(conf), (xyxy_[0], xyxy_[1]), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 1, cv2.LINE_AA)
+                            cv2.putText(im0, str(f'X:{xyxy_[0]} Y:{xyxy_[1]}'), (xyxy_[0]+(w/2), xyxy_[1]), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1, cv2.LINE_AA)
 
-                            # fx = x + (w / 2)
-                            fx = x +(w-x)/2
+                            fx = x + (w / 2)
                             fy = y + h  # 차 밑 부분 찍는게 맞음 450
 
                             # Homography를 통한 좌표 변환
