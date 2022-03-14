c004d589918b1344f777ff497a1b7951369c6b09
장병희 <jmkabc31@ajou.ac.kr>
2022-03-14 13:47:13 +0900

    add img2mp4

---
+++ img2mp4.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+import glob
+
+img_array = []
+img_list = glob.glob('runs/detect/MAP/*.jpg')
+img_list.sort()
+for filename in img_list:
+    img = cv2.imread(filename)
+    height, width, layers = img.shape
+    size = (width, height)
+    img_array.append(img)
+
+out = cv2.VideoWriter('MAP.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 10, size)
+
+for i in range(len(img_array)):
+    out.write(img_array[i])
+out.release()
