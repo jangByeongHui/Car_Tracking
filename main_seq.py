6d5cc23cfcd285134f3bf4fcda1de494d9004f26
장병희 <jmkabc31@ajou.ac.kr>
2022-03-14 09:50:40 +0900

    add first seq version

--- main_seq.py
+++ main_seq.py
@@ -13,140 +13,91 @@ FRANME_SYNC=-1
 new_car_index=0
 Map_path = "data/videos/B3.png"
 Map = cv2.imread(Map_path)
+window_width = 330
+window_height = 270
+# yolov5
+# 로컬 레포에서 모델 로드(yolov5s.pt 가중치 사용, 추후 학습후 path에 변경할 가중치 경로 입력)
+# 깃허브에서 yolov5 레포에서 모델 로드
+# model = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5s.pt',device=num%3)
+model = torch.hub.load('yolov5', 'custom', path='yolov5s.pt', source='local', device=0)
+# 검출하고자 하는 객체는 차량이기 때문에 coco data에서 검출할 객체를 차량으로만 특정(yolov5s.pt 사용시)
+model.classes = [2]
+model.conf = 0.5
+font = cv2.FONT_HERSHEY_SIMPLEX  # 글씨 폰트
+# 표시할 색상들
+COLORS = [(0, 0, 255), (255, 0, 0), (0, 255, 0), (255, 255, 0), (0, 255, 255), (100, 100, 100), (255, 0, 255),
+          (255, 69, 0), (173, 255, 47), (100, 149, 237), (148, 0, 211), (255, 105, 180), (244, 164, 96),
+          (240, 255, 255)]
+len_COLORS = len(COLORS)
 
-def getFrame(cctv_addr,cctv_name,return_dict):
-    font = cv2.FONT_HERSHEY_SIMPLEX  # 글씨 폰트
-    cap = cv2.VideoCapture(cctv_addr)
-    while True:
-        start_time = time.time()
-        ret,frame = cap.read()
-        end_time = time.time()
-        #print(f'Get {cctv_name} a frame - {round(end_time - start_time, 3)} s')
-        if ret:
-            return_dict['img'][cctv_name] = frame
-        else:
-            Error_image = np.zeros((720, 1920, 3), np.uint8)
-            cv2.putText(Error_image, "Video Not Found!", (20, 70), font, 1, (0, 0, 255), 3)  # 비디오 접속 끊어짐 표시
-            return_dict['img'][cctv_name] = Error_image
-            #retry
-            cap = cv2.VideoCapture(cctv_addr)
+def detect(cctv_name,img):
+    global window_height,window_width,font,model
+    h,w,c =img.shape
 
+    # 특정 구역에서만 Object 표시
+    black_img = np.zeros((h, w, c), dtype=np.uint8)
+    road_poly=np.array(cams[cctv_name]['road'])
+    black_img = cv2.fillPoly(black_img,[road_poly], (255, 255, 255))
+    black_img = cv2.cvtColor(black_img, cv2.COLOR_BGR2GRAY)
+    res, thr = cv2.threshold(black_img, 127, 255, cv2.THRESH_BINARY)
+    contours, his = cv2.findContours(thr, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    cnt=contours[0]
 
+    # 추론
+    bodys = model(img, size=640) # yolov5 추론
+    # 현재 이미지에서 변환된 좌표를 담을 이미지
+    points = []
 
-def detect(return_dict):
-    font = cv2.FONT_HERSHEY_SIMPLEX  # 글씨 폰트
-    # yolov5
-    # yolov5
-    # 로컬 레포에서 모델 로드(yolov5s.pt 가중치 사용, 추후 학습후 path에 변경할 가중치 경로 입력)
-    # 깃허브에서 yolov5 레포에서 모델 로드
-    # model = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5s.pt',device=num%3)
-    model = torch.hub.load('yolov5', 'custom', path='yolov5s.pt', source='local', device=0)
-    # 검출하고자 하는 객체는 차량이기 때문에 coco data에서 검출할 객체를 차량으로만 특정(yolov5s.pt 사용시)
-    model.classes = [2]
-    model.conf = 0.5
-    window_width=330
-    window_height=270
-    # CCTV 화면 정렬
-    for num,cctv_name in enumerate(cams.keys()):
-        cv2.namedWindow(cctv_name)
-        cv2.moveWindow(cctv_name,320*(num%6),270*(num//6))
+    # yolo5
+    for i in bodys.pandas().xyxy[0].values.tolist():
+        # 결과
+        x1, y1, x2, y2, conf, cls, name = int(i[0]), int(i[1]), int(i[2]), int(i[3]), i[4], i[5], i[6]
+        # 차량 하단 중심 좌표 표시
+        target_x = int((x1 + x2) / 2)  # 차량 중심 x 좌표
+        center_y = int((y1 + y2) / 2)  # 차량 중심 y 좌표
+        target_y = int(y2)  # 차량 하단 y 좌표
 
-    # CCTV 화면 추론
-    while True:
-
-        for cctv_name in cams.keys():
-            img = return_dict['img'][cctv_name]  # 추론할 이미지
-            h,w,c =img.shape
-
-            # 특정 구역에서만 Object 표시
-            black_img = np.zeros((h, w, c), dtype=np.uint8)
-            road_poly=np.array(cams[cctv_name]['road'])
-            black_img = cv2.fillPoly(black_img,[road_poly], (255, 255, 255))
-            black_img = cv2.cvtColor(black_img, cv2.COLOR_BGR2GRAY)
-            res, thr = cv2.threshold(black_img, 127, 255, cv2.THRESH_BINARY)
-            contours, his = cv2.findContours(thr, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-            cnt=contours[0]
-            # 추론
-            start_time=time.time()
-            bodys = model(img, size=640) # yolov5 추론
-            end_time=time.time()
-            # print(f'yolov5 {cctv_name} img 추론 시간 - {round(end_time - start_time, 3)} s')
-            flag = False
-            points = []
-
-            # yolo5
-            for i in bodys.pandas().xyxy[0].values.tolist():
-                # 결과
-                x1, y1, x2, y2, conf, cls, name = int(i[0]), int(i[1]), int(i[2]), int(i[3]), i[4], i[5], i[6]
-                # 차량 하단 중심 좌표 표시
-                target_x = int((x1 + x2) / 2)  # 차량 중심 x 좌표
-                center_y = int((y1 + y2) / 2)  # 차량 중심 y 좌표
-                target_y = int(y2)  # 차량 하단 y 좌표
-
-                # 차량이 도로안에 있는 것만 검출
-                if cv2.pointPolygonTest(cnt,(target_x,center_y),False)>-1:  # 도로로 표시한 polygon안에 있는 경우에만 검출 차량이 도로에 있으면 1 없으면 -1 정확히 겹치면 0
-                    cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)  # bounding box
-                    cv2.putText(img, name, (x1 - 5, y1 - 5), font, 0.5, (255, 0, 0), 1)  # class 이름
-                    cv2.putText(img, "{:.2f}".format(conf), (x1 + 5, y1 - 5), font, 0.5, (255, 0, 0), 1)  # 정확도
+        # 차량이 도로안에 있는 것만 검출
+        if cv2.pointPolygonTest(cnt,(target_x,center_y),False)>-1:  # 도로로 표시한 polygon안에 있는 경우에만 검출 차량이 도로에 있으면 1 없으면 -1 정확히 겹치면 0
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)  # bounding box
+            cv2.putText(img, name, (x1 - 5, y1 - 5), font, 0.5, (255, 0, 0), 1)  # class 이름
+            cv2.putText(img, "{:.2f}".format(conf), (x1 + 5, y1 - 5), font, 0.5, (255, 0, 0), 1)  # 정확도
 
 
-                    # 보행자 픽셀 위치 표시
-                    img = cv2.circle(img, (target_x, target_y), 10, (255, 0, 0), -1)
-                    cv2.putText(img, "X:{} y:{}".format(target_x + 5, target_y + 5), (target_x + 10, target_y + 10), font,
-                                0.5, (255, 0, 255), 1)
+            # 보행자 픽셀 위치 표시
+            img = cv2.circle(img, (target_x, target_y), 10, (255, 0, 0), -1)
+            cv2.putText(img, "X:{} y:{}".format(target_x + 5, target_y + 5), (target_x + 10, target_y + 10), font,
+                        0.5, (255, 0, 255), 1)
 
-                    # homography 변환
-                    target_point = np.array([target_x, target_y, 1], dtype=int)
-                    target_point.T
-                    H = np.array(cams[cctv_name]['homoMat'])
-                    target_point = H @ target_point
-                    target_point = target_point / target_point[2]
-                    target_point = list(target_point)
-                    target_point[0] = round(int(target_point[0]), 0)  # x - > left
-                    target_point[1] = round(int(target_point[1]), 0)  # y - > top
-                    points.append((target_point[0], target_point[1])) #Homography로 계산 좌표 값
-                    flag = True  # 변환된 정보 저장
-                #Yolov5 추론 끝
-
-            # 변환된 보행자 픽셀 위치 저장
-            if flag:
-                return_dict[cctv_name] = (flag, points)
-            else:
-                return_dict[cctv_name] = (False, [])
-            temp_img = cv2.resize(img, dsize=(window_width, window_height))
-            cv2.imshow(cctv_name, temp_img)
-            cv2.imwrite("runs/video/"+cctv_name+".jpg",temp_img)
-        #send2server(return_dict)
-        Stich_Car(return_dict)
-        k = cv2.waitKey(1) & 0xff
-        if k == 27:
-            break
+            # homography 변환
+            target_point = np.array([target_x, target_y, 1], dtype=int)
+            target_point.T
+            H = np.array(cams[cctv_name]['homoMat'])
+            target_point = H @ target_point
+            target_point = target_point / target_point[2]
+            target_point = list(target_point)
+            target_point[0] = round(int(target_point[0]), 0)  # x - > left
+            target_point[1] = round(int(target_point[1]), 0)  # y - > top
+            points.append((target_point[0], target_point[1])) #Homography로 계산 좌표 값
+    resize_img = cv2.resize(img, dsize(window_height, window_width))
+    cv2.imshow(cctv_name, resize_img)
+    return points
 
 def finddistance(x1,y1,x2,y2):
     a = np.array((x1, y1,0))
     b = np.array((x2, y2, 0))
     return np.sqrt(np.sum(np.square(a-b)))
 
-def Stich_Car(data):
-    global new_car_index
-    global track_point
-    global FRANME_SYNC
-    global Map
-    font = cv2.FONT_HERSHEY_SIMPLEX  # 글씨 폰트
-    # 표시할 색상들
-    COLORS = [(0,0,255),(255,0,0),(0,255,0),(255,255,0),(0,255,255),(100,100,100),(255,0,255),(255,69,0),(173,255,47),(100,149,237),(148,0,211),(255,105,180),(244,164,96),(240,255,255)]
-    len_COLORS=len(COLORS)
+def Stich_Car(points):
+    global new_car_index,track_point,FRANME_SYNC,Map,font,COLORS,len_COLORS
     temp_points = [] # 여러 CCTV에 의해 중복되는 좌표를 제거 후 담을 좌표들
     all_temp_points =[] # 지도에 표시할 모든 좌표를 담을 좌표들
-    threshold_dist = 400
+    threshold_dist = 400 #일정 거리 이하일시 같은 좌표로 판단
     temp_trackpoints=[] # 현재 추론된 차량 트랙킹 정보를 담을 리스트
 
-    # 동일한 프레임 시간대에 지도에 표시되는 모든 좌표 저장
-    for cctv_name in cams.keys(): # 모든 CCTV에서 좌표 가져오기
-        flag,points = data[cctv_name] #현재 CCTV에서 좌표 정보가 있는지
-        #초기 모든 지도 죄표 저장
-        for (pX,pY) in points:
-            all_temp_points.append((pX,pY))
+    #초기 모든 지도 죄표 저장
+    for (pX,pY) in points:
+        all_temp_points.append((pX,pY))
 
     # 추론된 좌표들 중에서 이미 같은 것이라고 판단된 좌표들은 삭제
     for (aX,aY) in all_temp_points:
@@ -195,7 +146,7 @@ def Stich_Car(data):
         now_TIME="%04d%02d%02d_%02d%02d%02d"%(now.tm_year,now.tm_mon,now.tm_mday,now.tm_hour,now.tm_min,now.tm_sec)
         cv2.imwrite(f'runs/detect/MAP/{now_TIME}.jpg',temp_Map)
 
-    #최대 5개의 이전 프레임 기록을 저장
+    #최대 10개의 이전 프레임 기록을 저장
     FRANME_SYNC=(FRANME_SYNC+1)%10 #FRAME_SYNC는 0~9 값을 가지고 이전 기록을 계속해서 저장
     track_point[FRANME_SYNC]=temp_trackpoints #기존 트랙킹하는 좌표안에 임시로 저장한 좌표들 저장
 
@@ -229,33 +180,39 @@ def send2server(data):
 
 
 def main():
-    # 작업 결과 저장 dict
-    manager = multiprocessing.Manager()
-    return_dict = manager.dict()
-    return_dict['img'] = manager.dict()
+    # 초기 설정 값
     test_videos=["data/CCTV_02.mp4","data/CCTV_10.mp4","data/CCTV_11.mp4","data/CCTV_12.mp4","data/CCTV_17.mp4","data/CCTV_18.mp4","data/CCTV_19.mp4","data/CCTV_20.mp4","data/CCTV_21.mp4","data/CCTV_22.mp4","data/CCTV_23.mp4","data/CCTV_24.mp4"]
-    #init
+
+    #초기 빈 화면으로 init
     for cctv_name in cams.keys():
         return_dict['img'][cctv_name] = np.zeros((1080, 1920, 3), np.uint8)
-    work_lists=[]
-    jobs=[]
 
-    # 멀티 프로세싱할 arguments 생성
-    for num,cctv_name in enumerate( cams.keys()):
-        work_lists.append((test_videos[num],cctv_name,return_dict))
+    # CCTV 화면 정렬
+    for num, cctv_name in enumerate(cams.keys()):
+        cv2.namedWindow(cctv_name)
+        cv2.moveWindow(cctv_name, window_width * (num % 6), window_height * (num // 6))
 
-    #프로세스 실행
-    for i,work in enumerate(work_lists):
-        p = multiprocessing.Process(target=getFrame, args=work)
-        jobs.append(p)
-        p.start()
-    else:
-        p = multiprocessing.Process(target=detect, args=(return_dict,))
-        jobs.append(p)
-        p.start()
+    #videocapture 객체 저장
+    for index,cctv_name in enumerate(cams.keys()):
+        cams[cctv_name]['cap']=cv2.VideoCapture(test_videos[index])
 
-    for proc in jobs:
-        proc.join()
+    while True:
+        MAP_Points =[]
+        for cctv_name in cams.keys():
+            #초기 빈화면
+            img = np.zeros((720, 1920, 3), np.uint8)
+            cv2.putText(Error_image, "Video Not Found!", (20, 70), font, 1, (0, 0, 255), 3)  # 비디오 접속 끊어짐 표시
+            ret,frame = cams[cctv_name]['cap'].read() #각각 CCTV별로 이미지 가져오기
+            if ret:
+                img=frame
+            else:
+                cams[cctv_name]['cap'].release() #videocapture 객체 프리
+            new_points = detect(cctv_name,img) # 추론한 좌표 가져오기
+            MAP_Points.extend(new_points) # 추론 좌표 저장
+        Stich_Car(MAP_Points)
+
+
+
 
 if __name__ == '__main__':
     main()
