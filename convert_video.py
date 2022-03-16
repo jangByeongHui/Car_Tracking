d72ef5b05af347a93ec6b56cb1da9910528a7ebe
장병희 <jmkabc31@ajou.ac.kr>
2022-03-16 09:29:46 +0900

    typo

--- convert_video.py
+++ convert_video.py
@@ -3,7 +3,7 @@ import cv2
 video_path="12.mp4"
 
 
-cv2.VideoCapture(video_path)
+cap=cv2.VideoCapture(video_path)
 out = cv2.VideoWriter(f'{video_path[:-4]}_convert.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 15, (1920,1080))
 
 while True:
