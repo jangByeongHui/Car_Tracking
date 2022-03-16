cec928d9497e9ffb4940fddfc71c1f519eb45420
장병희 <jmkabc31@ajou.ac.kr>
2022-03-16 14:17:30 +0900

    save points

--- restore_points.py
+++ restore_points.py
@@ -1,16 +1,24 @@
 import pickle
 import cv2
-MAP_PATH = "data/B3.png"
-list = []
 
-with open('points.pkl','rb') as f:
+MAP_PATH = "data/B3.png"
+
+with open('return_points.pkl','rb') as f:
     list = pickle.load(f)
 
-Map = cv2.imread(MAP_PATH)
+list.sort()
+with open('return_points.pkl','wb') as f:
+    list = pickle.dump(list,f)
 
+Map = cv2.imread(MAP_PATH)
+h,w,c = Map.shape
+out = cv2.VideoWriter('Tracking.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 15, (w,h))
 for i in list:
     frames,points = i
     Map = cv2.circle(Map, (points[0], points[1]), 30, (0, 255, 0), -1)  # 지도위에 표시
+    out.write(Map)
     cv2.imshow("MAP",Map)
     cv2.waitKey(1)
+else:
+    out.release()
 
